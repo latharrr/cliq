2431b5ba5e994b83b48d3da3671a3d1902aa60b7
Deepanshu Lathar <101576802+latharrr@users.noreply.github.com>
2026-03-22 11:09:42 +0530

    feat(android): unlock 120fps refresh rate overriding 60hz battery saver mode

--- android/app/src/main/java/com/lpu/cliq/MainActivity.java
+++ android/app/src/main/java/com/lpu/cliq/MainActivity.java
@@ -1,5 +1,32 @@
 package com.lpu.cliq;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Force Maximum Refresh Rate (120Hz/144Hz) for smoother scrolling
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Display.Mode[] modes = getWindow().getWindowManager().getDefaultDisplay().getSupportedModes();
+            Display.Mode maxMode = null;
+            float maxFPS = 0f;
+            for (Display.Mode mode : modes) {
+                if (mode.getRefreshRate() > maxFPS) {
+                    maxFPS = mode.getRefreshRate();
+                    maxMode = mode;
+                }
+            }
+            if (maxMode != null) {
+                WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+                layoutParams.preferredDisplayModeId = maxMode.getModeId();
+                getWindow().setAttributes(layoutParams);
+            }
+        }
+    }
+}
